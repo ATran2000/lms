f86cebc6996322c2e5dd6b95c3b0f4f138622804
swegroup81 <117475575+swegroup81@users.noreply.github.com>
2022-12-05 22:08:09 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,6 +20,16 @@ app.listen(PORT, console.log(`Server started on port ${PORT}`));
 // API CODE
 app.use(express.json({ limit: '1mb' }));
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '/../client/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
+    });
+  }
+
 // API routing to virtual database
 app.post('/api', async (request, response) => {
   console.log('I got a request to test!');
